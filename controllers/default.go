7f295927c14e3dc575ccb61dfc7755abcdc942c0
leiwen <leiwen@jmfen.cn>
2020-07-01 19:06:00 +0800

    feat：完成MySQL数据库的操作

--- controllers/default.go
+++ controllers/default.go
@@ -12,7 +12,7 @@ type MainController struct {
 func (c *MainController) Get() {
 	//c.Data["Website"] = "beego.me"
 	//c.Data["Email"] = "astaxie@gmail.com"
-
+	models.UpdatePage()
 	m := models.GetPage()
 	c.Data["Website"] = m.Website
 	c.Data["Email"] = m.Email
--- models/page.go
+++ models/page.go
@@ -17,7 +17,7 @@ func init() {
 	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/godb?charset=utf8",30)
 	orm.RegisterModel(new(Page))
 }
-
+/*获取数据*/
 func GetPage() Page {
 	//rtn := Page{Website:"leixiaotian.cn",Email:"1124378213@qq.com"}
 	//return rtn
@@ -27,4 +27,11 @@ func GetPage() Page {
 	fmt.Println(err)
 
 	return p
+}
+
+/*更新数据*/
+func UpdatePage(){
+	p := Page{Id:1,Email:"myemail_updated"}
+	o := orm.NewOrm()
+	o.Update(&p,"Email")
 }
