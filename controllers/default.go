d23057453f4371639e0a1c315c7f731838594810
leiwen <leiwen@jmfen.cn>
2020-07-01 18:55:23 +0800

    init project

---
+++ controllers/default.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"lxtkj/hellobeego/models"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	//c.Data["Website"] = "beego.me"
+	//c.Data["Email"] = "astaxie@gmail.com"
+
+	m := models.GetPage()
+	c.Data["Website"] = m.Website
+	c.Data["Email"] = m.Email
+	c.TplName = "index.tpl"
+}
---
+++ models/page.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"fmt"
+
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Page struct {
+	Id      int
+	Website string
+	Email   string
+}
+
+func init() {
+	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/godb?charset=utf8",30)
+	orm.RegisterModel(new(Page))
+}
+
+func GetPage() Page {
+	//rtn := Page{Website:"leixiaotian.cn",Email:"1124378213@qq.com"}
+	//return rtn
+	o := orm.NewOrm()
+	p := Page{Id: 1}
+	err := o.Read(&p)
+	fmt.Println(err)
+
+	return p
+}
