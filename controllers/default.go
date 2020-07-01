185c2da289365f244302afe43f5ce58a500c0142
leiwen <leiwen@jmfen.cn>
2020-07-01 19:21:41 +0800

    feat：完成session合日志功能

--- controllers/default.go
+++ controllers/default.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 	"lxtkj/hellobeego/models"
 )
 
@@ -12,7 +14,17 @@ type MainController struct {
 func (c *MainController) Get() {
 	//c.Data["Website"] = "beego.me"
 	//c.Data["Email"] = "astaxie@gmail.com"
+
+	//session使用
+	c.SetSession("username", "leixiaotian")
+	user := c.GetSession("username")
+	logs.Informational("user you loged in")
+	fmt.Println(user)
+
+	//更新邮箱
 	models.UpdatePage()
+
+	//查询数据并渲染
 	m := models.GetPage()
 	c.Data["Website"] = m.Website
 	c.Data["Email"] = m.Email
--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	_ "lxtkj/hellobeego/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 )
 
 func main() {
+	logs.SetLevel(beego.LevelInformational)
+	logs.SetLogger("file",`{"filename":"logs/test.log"}`)
 	beego.Run()
 }
 
