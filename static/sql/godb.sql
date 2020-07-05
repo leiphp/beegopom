9fdf2487396b840270f2dfebf9581798155903cf
leiwen <657106593@qq.com>
2020-07-05 11:45:07 +0800

    feat：mysql

--- static/sql/godb.sql
+++ static/sql/godb.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
+Source Server         : 本地
 Source Server Version : 50726
 Source Host           : localhost:3306
 Source Database       : godb
@@ -10,10 +10,32 @@ Target Server Type    : MYSQL
 Target Server Version : 50726
 File Encoding         : 65001
 
-Date: 2020-07-04 19:40:08
+Date: 2020-07-05 11:44:34
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for data
+-- ----------------------------
+DROP TABLE IF EXISTS `data`;
+CREATE TABLE `data` (
+  `did` int(11) NOT NULL AUTO_INCREMENT,
+  `mid` int(11) NOT NULL DEFAULT '0',
+  `parent` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(60) NOT NULL DEFAULT '',
+  `content` varchar(2048) NOT NULL DEFAULT '{}',
+  `seq` int(11) NOT NULL DEFAULT '0',
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `update_time` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`did`),
+  KEY `data_seq` (`seq`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of data
+-- ----------------------------
+INSERT INTO `data` VALUES ('1', '9', '0', '小明22', '{\n  \"parent\": 0,\n  \"name\": \"小明22\",\n  \"seq\": \"0\",\n  \"status\": \"0\",\n  \"username\": \"学生姓名\",\n  \"sex\": \"male\",\n  \"class\": 3\n}', '0', '0', '1593919514');
 
 -- ----------------------------
 -- Table structure for menu
@@ -37,9 +59,9 @@ INSERT INTO `menu` VALUES ('3', '1', '购物车', '1', '{\"schema\":{\"name\":{\
 INSERT INTO `menu` VALUES ('4', '1', '会员管理', '0', '');
 INSERT INTO `menu` VALUES ('5', '0', '书城', '0', '');
 INSERT INTO `menu` VALUES ('6', '5', '图书管理', '0', '');
-INSERT INTO `menu` VALUES ('7', '0', '学生信息管理', '0', '');
-INSERT INTO `menu` VALUES ('8', '7', '班级信息', '0', '');
-INSERT INTO `menu` VALUES ('9', '7', '学生信息', '0', '');
+INSERT INTO `menu` VALUES ('7', '0', '学生信息管理', '0', '{\"schema\":{\"name\":{\"type\":\"string\",\"title\":\"姓名\"},\"class\":{\"type\":\"number\",\"title\":\"班级\"},\"sex\":{\"type\":\"string\",\"title\":\"性别\",\"enum\":[\"unkown\",\"male\",\"female\"]}},\"form\":[{\"key\":\"name\"},{\"key\":\"sex\"},{\"key\":\"class\"}]}');
+INSERT INTO `menu` VALUES ('8', '7', '班级信息', '0', '{\"schema\":{\"name\":{\"type\":\"string\",\"title\":\"姓名\"},\"class\":{\"type\":\"number\",\"title\":\"班级\"},\"sex\":{\"type\":\"string\",\"title\":\"性别\",\"enum\":[\"unkown\",\"male\",\"female\"]}},\"form\":[{\"key\":\"name\"},{\"key\":\"sex\"},{\"key\":\"class\"}]}');
+INSERT INTO `menu` VALUES ('9', '7', '学生信息', '0', '{\"schema\":{\"username\":{\"type\":\"string\",\"title\":\"姓名\"},\"class\":{\"type\":\"number\",\"title\":\"班级\"},\"sex\":{\"type\":\"string\",\"title\":\"性别\",\"enum\":[\"unkown\",\"male\",\"female\"]}},\"form\":[{\"key\":\"username\"},{\"key\":\"sex\"},{\"key\":\"class\"}]}');
 
 -- ----------------------------
 -- Table structure for page
